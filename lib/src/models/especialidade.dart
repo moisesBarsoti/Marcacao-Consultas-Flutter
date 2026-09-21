44231a4806213f6333a1500ca44bab373bf67d52
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-21 13:46:50 -0300

    feat: Persiste a lista de consultas e confirma pelo id

--- lib/src/models/especialidade.dart
+++ lib/src/models/especialidade.dart
@@ -6,6 +6,18 @@ class Especialidade {
   const Especialidade({
     required this.id,
     required this.nome,
-    required this.descricao
+    required this.descricao,
   });
+
+  factory Especialidade.fromJson(Map<String, dynamic> json) {
+    return Especialidade(
+      id: json['id'] as int,
+      nome: json['nome'] as String,
+      descricao: json['descricao'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'nome': nome, 'descricao': descricao};
+  }
 }
--- lib/src/models/paciente.dart
+++ lib/src/models/paciente.dart
@@ -10,6 +10,26 @@ class Paciente {
     required this.nome,
     required this.cpf,
     required this.email,
-    this.telefone
+    this.telefone,
   });
+
+  factory Paciente.fromJson(Map<String, dynamic> json) {
+    return Paciente(
+      id: json['id'] as int,
+      nome: json['nome'] as String,
+      cpf: json['cpf'] as String,
+      email: json['email'] as String,
+      telefone: json['telefone'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'nome': nome,
+      'cpf': cpf,
+      'email': email,
+      'telefone': telefone,
+    };
+  }
 }
